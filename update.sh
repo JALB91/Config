6b2331dd1523d251152e6fc0f71f013cf1ca9f26
Damiano Calcagni <damiano.calcagni@redbitgames.it>
2017-01-31 18:12:51 +0100

    Qualcosa

--- update.sh
+++ update.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+cp `dirname $0`/VimConfig/vimrc $HOME/.vimrc
+cp -r `dirname $0`/VimConfig/vim $HOME/.vim
+cp `dirname $0`/VimConfig/ycm_extra_conf.py $HOME/.ycm_extra_conf.py
 
+cp `dirname $`
