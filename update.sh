5be5404901599ecb855afe02abc6214754faa09d
JALB91 <damiano.calcagni@gmail.com>
2017-02-09 19:25:17 +0100

    Modificato bashrc e update.sh

--- update.sh
+++ update.sh
@@ -4,4 +4,5 @@ cp `dirname $0`/VimConfig/vimrc $HOME/.vimrc
 cp -r `dirname $0`/VimConfig/vim $HOME/.vim
 cp `dirname $0`/VimConfig/ycm_extra_conf.py $HOME/.ycm_extra_conf.py
 
-cp `dirname $`
+cp `dirname $0`/BashConfig/bashrc $HOME/.bashrc
+cp `dirname $0`/BashConfig/inputrc $HOME/.inputrc
